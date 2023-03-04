5b3e7dc4bc1a9b5994398cd631e1a732f0a9c7f7
ganeshd588 <126935393+ganeshd588@users.noreply.github.com>
2023-03-04 10:22:39 +0530

    SAMPLE OUTPUT:
    enter a5
    enter b6
    enter c7
    maximum is c

---
+++ maximum of 3 numbers.py
@@ -0,0 +1,68 @@
+a=int(input("enter a"))
+b=int(input("enter b"))
+c=int(input("enter c"))
+if(a>b and b>c):
+    print("maximum is a")
+elif(b>a and b>c):
+    print("maximum is b ")
+
+else:
+    print("maximum is c")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
